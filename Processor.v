4aacd3ddc318e44b0e0fd13ceef9a32ef3ac0aaf
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:38:23 +0530

    Update Processor.v

--- Processor.v
+++ Processor.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.09.2024 06:02:32
-// Design Name: 
-// Module Name: Processor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 // Using Define Directive (use GLOBAL INCLUDE for other files to acess the define directives used here)
 // Fields of IR
